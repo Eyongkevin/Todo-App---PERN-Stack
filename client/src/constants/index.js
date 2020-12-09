fd48f70bd462dbb8f1084967970cb53302e71ffc
Eyong Kevin <tonyparkerkenz@gmail.com>
2020-12-09 11:31:32 +0100

    Update colors for todochecklist
    Removed bright colors and added more lighter colors.

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,15 +1,13 @@
 // all posible status used in this app
-export const STATUS = ['Task','Do Today','In Progress','Done','Stuck']
-export const API_URL = 'http://localhost:5000'
-export const SERVER_ERROR_MESSAGE = "Server response wasn't OK! "
+export const STATUS = ['Task','Do Today','In Progress','Done','Stuck'];
+export const API_URL = 'http://localhost:5000';
+export const SERVER_ERROR_MESSAGE = "Server response wasn't OK! ";
 
 export const TASK_COLORS = [
     '#f0f8ff',
-    '#faebd7', 
-    '#7fffd4',	
+    '#faebd7', 	
     '#f0ffff',
     '#f5f5dc',
-    '#ffe4c4',
     '#ffebcd',
     '#FFFAFA',
     '#FFFFFF',
@@ -27,7 +25,15 @@ export const TASK_COLORS = [
     '#FAEBD7', 	
     '#FAF0E6', 	
     '#FFF0F5', 	
-    '#FFE4E1'
-]
+    '#FFE4E1',
+    '#fdfbf2',
+    '#f2fdf4',
+    '#fdf6f2',
+    '#f2fdfd',
+    '#f2f9fd',
+    '#f2f6fd',
+    '#fbf2fd',
+    '#fdf2f4'
+];
 
 
